70a897de67f3daeb7d7b40f2e6abf4ce81971b56
Tobias Heintz <thz@plista.com>
2013-08-19 15:22:31 +0200

    simplified onsite handler massively, modified fetch() of model so it does not call the handler again, formatting

--- classes/Plista/Orp/Sdk/Example/ExampleUniversityFetchOnsiteHandler.php
+++ classes/Plista/Orp/Sdk/Example/ExampleUniversityFetchOnsiteHandler.php
@@ -1,8 +1,9 @@
 <?php
 namespace Plista\Orp\Sdk\Example;
 use Plista\Orp\Sdk\Handle;
+use Plista\Orp\Sdk\Recs;
 
-class ExampleUniversityFetchOnsiteHandler  implements Handle /*extends \Plista\Orp\Algorithm\Base\FetchOnsite*/ {
+class ExampleUniversityFetchOnsiteHandler implements Handle {
 
 	const SCORE = 2;
 	const ITEM = 3;
@@ -14,49 +15,17 @@ class ExampleUniversityFetchOnsiteHandler  implements Handle /*extends \Plista\O
 		$limit = $request['limit'];
 
 		$model = new ExampleUniversityModel();
-		$model->fetch($request, $limit);
-	}
+		$result = $model->fetch($request, $limit);
 
-
-	private $data;
-
-	public function fetch($item_id, $result) {
-		// collecting item_id and result (recommendation)
-		$this->data['recs']['ints'][self::ITEM] = $result;
-		$this->data['recs']['floats'][self::SCORE] = $item_id;
-		return $this->data;
-	}
-
-	public function fetchOnsite($item_id, $result) {
-		// building structure of json response
-		$object = $this->fetch($item_id, $result);
-		// wrapping things up and getting ready to transmit
-		$recommendation_proposal = $this->getPostData($object);
-		//providing recommendation to plista
-		print ($recommendation_proposal);
-	}
-
-	public function toJSON($object) {
-		// encoding recommendation
-		$json_string = json_encode($object);
-
-		if ($json_string === false) {
-			throw new Exception('Error: Could not encode response to JSON :( .');
-		}
-		return $json_string;
-	}
-
-	public function getPostData($object) {
-		// returning json string
-		return $this->toJSON($object);
+		$recs = new Recs($result);
+		return $recs;
 	}
 
 	public function validate($data) {
 		if (empty($data)) {
 			throw new Exception('Error: request is empty');
-		} else {
-			return true;
 		}
-	}
 
+		return true;
+	}
 }
--- classes/Plista/Orp/Sdk/Example/ExampleUniversityModel.php
+++ classes/Plista/Orp/Sdk/Example/ExampleUniversityModel.php
@@ -1,6 +1,7 @@
 <?php
 namespace Plista\Orp\Sdk\Example;
-class ExampleUniversityModel  {
+
+class ExampleUniversityModel {
 	/*
 	*  In order to write the array in the specified file, its important to serialize() the array.
 	*  If you want to file_get_contents be sure to unserialize() the content again.
@@ -10,26 +11,26 @@ class ExampleUniversityModel  {
 	const ITEM = 3;
 
 	// if you want to save the files in a specific directory, you may want to adapt $path
-	public $path = '';  // z.B. /home/user/plista/orp/
+	public $path = ''; // e.g. /home/user/plista/orp/
 
-	public function write_item($item_id , $publisherid) {
+	public function write_item($item_id, $publisherid) {
 
 		$today = date("m.d.y");
 		// writing items in file
-		$res = file_put_contents($this->path . $publisherid . '_items_'. $today . '.txt',  $item_id .  "\n", FILE_APPEND | LOCK_EX );
+		$res = file_put_contents($this->path . $publisherid . '_items_' . $today . '.txt', $item_id . "\n", FILE_APPEND | LOCK_EX);
 
 		if (!$res) {
-			die ('Error: Unable to write to item file :(');
+			throw new Exception('Error: Unable to write to item file :(');
 		}
 	}
 
 	public function write_publisherId($publisherid) {
 		// writing the current item_id in file
 		// the file is supposed to contain only the last ID
-		$res = file_put_contents($this->path . 'publisherID.txt',   $publisherid,  LOCK_EX );
+		$res = file_put_contents($this->path . 'publisherID.txt', $publisherid, LOCK_EX);
 
 		if (!$res) {
-			die ('Error: Unable to write to publisherID file :(');
+			throw new Exception('Error: Unable to write to publisherID file :(');
 		}
 	}
 
@@ -37,59 +38,60 @@ class ExampleUniversityModel  {
 
 		$today = date("m.d.y");
 		// writing statistics in file
-		$res = file_put_contents($this->path . 'statistic_'. $today . '.txt', serialize($seq).  "\n", FILE_APPEND | LOCK_EX );
+		$res = file_put_contents($this->path . 'statistic_' . $today . '.txt', serialize($seq) . "\n", FILE_APPEND | LOCK_EX);
 
 		if (!$res) {
-			die ('Error: Unable to write to statistic file :(');
+			throw new Exception('Error: Unable to write to statistic file :(');
 		}
 	}
 
 	public function write_error($error) {
 		$today = date("m.d.y");
 		// writing errors in log file
-		$res = file_put_contents($this->path . 'error_' . $today . '.txt',   serialize($error).  "\n", FILE_APPEND | LOCK_EX  );
+		$res = file_put_contents($this->path . 'error_' . $today . '.txt', serialize($error) . "\n", FILE_APPEND | LOCK_EX);
 		if (!$res) {
-			die ('Error: Unable to write to error file :(');
+			throw new Exception('Error: Unable to write to error file :(');
 		}
 	}
 
 	public function write_request($request) {
-		/* optional - if all request should be save to a file
+		// if all request should be save to a file
 		$today = date("m.d.y");
 		// writing requests in file
-		$res = file_put_contents($this->path . 'request_'. $today . '.txt',   serialize($request).  "\n", FILE_APPEND | LOCK_EX  );
+		$res = file_put_contents($this->path . 'request_' . $today . '.txt', serialize($request) . "\n", FILE_APPEND | LOCK_EX);
 
 		if (!$res) {
-			die ('Error: Unable to write to request file :(');
+			throw new Exception('Error: Unable to write to request file :(');
 		}
-		*/
 	}
-
 
 	public function getPublisherId() {
 		// reading the last item ID from file
-		$publisherid  = file_get_contents($this->path . 'publisherID.txt');
+		$publisherid = file_get_contents($this->path . 'publisherID.txt');
 		if (!$publisherid) {
-			die ('Error: Unable to write to request file :(');
+			throw new Exception('Error: Unable to write to request file :(');
 		}
 		return $publisherid;
 	}
 
+	/**
+	 * @param $request
+	 * @param $limit
+	 * @throws Exception
+	 * @return array
+	 */
 	public function fetch($request, $limit) {
 		// record the request in file
-		$this->write_request($request);
-		// getting item_id
-		$publisherid = $this->getPublisherId();
-		if (!$publisherid) {
-			die ('Error: $item_id is not supposed to be null :(');
-		}
+		//$this->write_request($request); // uncomment this line to have all requests logged to file
+
 		// using algorithm in order to generate a recommendation
 		$item = $this->algorithm_item($request, $limit);
 		$score = $this->algorithm_score($request, $limit);
 
-		$fetchOnsiteHandler = new ExampleUniversityFetchOnsiteHandler();
-		$fetchOnsiteHandler->fetchOnsite($score, $item);
-
+		return array(
+			'result' => $item,
+			'score'  => $score
+		);
 	}
 
 	/*
@@ -98,46 +100,61 @@ class ExampleUniversityModel  {
 	 * http://en.wikipedia.org/wiki/Collaborative_filtering
 	 * http://en.wikipedia.org/wiki/Behavioral_targeting
 	 */
-	public function  algorithm_score($request, $limit) {
+	public function algorithm_score($request, $limit) {
 		// here goes your algorithm
 		$c = 0;
 		$score = array();
+
 		// getting decreasing score
-		while($c < $limit) {
+		while ($c < $limit) {
 			// handling if $c==0 to avoid dividing with 0
-			if ($c==0) {
-				$v =1;
+			if ($c == 0) {
+				$v = 1;
 			} else {
-			$v = 1/$c;
+				$v = 1 / $c;
 			}
 			$score[] = $v;
 			$c++;
 		}
+
 		return $score;
 	}
 
+	/**
+	 * Example implementation of an algorithm. We only get the most recent items.
+	 * @param $request
+	 * @param $limit
+	 * @return array
+	 * @throws Exception
+	 */
 	public function algorithm_item($request, $limit) {
 		// here goes your algorithm
 
 		$item = array();
 		$today = date("m.d.y");
+		// TODO: get publisher id from context, not from file
 		$publisherid = $this->getPublisherId();
+
 		if (!$publisherid) {
-			die ('Error: $item_id is not supposed to be null :(');
+			throw new Exception('Error: $item_id is not supposed to be null :(');
 		}
-		$file = file($this->path . $publisherid . '_items_'. $today . '.txt');
-		$c = count($file)-$limit;
+
+		$file = file($this->path . $publisherid . '_items_' . $today . '.txt');
+		$c = count($file) - $limit;
 		$tc = count($file);
+
 		// if the item file is empty or has less then $limit entries, PHP error will occur
 		if ($tc < $limit) {
-			throw new Exception ('Error: not enough entries in item file :(');
+			throw new Exception('Error: not enough entries in item file :(');
 		}
+
 		// getting the last $limit entries of file
 		for (; $c < $tc; $c++) {
 			$object = $file[$c];
 			// getting rid of "\n"
 			$item[] = rtrim($object);
 		}
+
 		return $item;
 	}
 }
