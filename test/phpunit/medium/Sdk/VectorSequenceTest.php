b39532c720c2e5a9a881d06042f6249085aa8d59
Tobias Heintz <thz@plista.com>
2013-08-19 15:35:24 +0200

    removed unused controller test, proper parent class, no need for setUp() and tearDown(), formatting

--- test/phpunit/medium/Sdk/VectorSequenceTest.php
+++ test/phpunit/medium/Sdk/VectorSequenceTest.php
@@ -3,25 +3,12 @@
 namespace PlistaTest\Orp\medium\Sdk;
 use Plista\Orp\Sdk;
 
-
 require_once __DIR__ . '/../../../bootstrap.php';
 
-/**
- *
- */
-class VectorSequenceTest  {
-
-	protected function setUp() {
-		parent::setUp();
-	}
-
-	protected function tearDown() {
-		parent::tearDown();
-	}
+class VectorSequenceTest extends \PHPUnit_Framework_TestCase {
 
 	public function testFromJson() {
-
-		$example= '{
+		$example = '{
 			"type": "impression",
 			"context": {
 				"simple": {
@@ -85,44 +72,41 @@ class VectorSequenceTest  {
 			"timestamp": 1372175999641
 			}';
 
-
-
 		$object = \Plista\Orp\Sdk\VectorSequence::fromJson($example);
 
 		/**
 		 * testing Type values
 		 */
-		$this->assertEquals($object->getType()->getValue(), impression);
+		$this->assertEquals($object->getType()->getValue(), 'impression');
 
 		/**
 		 * testing context values
 		 */
-		$this->assertEquals($object->getContext()->getGender(), array(7 => 255));    	//1
-		$this->assertEquals($object->getContext()->getAge(), array(12 , 13 , 42 , 90 , 46 , 29 , 19)); //2
-		$this->assertEquals($object->getContext()->getIncome(), array(43 , 24 , 44 , 105 , 20 , 16)); //3
-		$this->assertEquals($object->getContext()->getBrowser(), 312613);  		  //4
-		$this->assertEquals($object->getContext()->getIsp(), 86);   	    	  	 //5
-		$this->assertEquals($object->getContext()->getOs(), 431247);   	    		 //6
-		$this->assertEquals($object->getContext()->getGeo_user(), null);     		 //7
-		$this->assertEquals($object->getContext()->getPublisher_filter(), array(18841, 18842, 48511));         //8
-		$this->assertEquals($object->getContext()->getGeo_user(), null);    		 //9
-		$this->assertEquals($object->getContext()->getChannel(), array(9, 10)); 	//10
-		$this->assertEquals($object->getContext()->getCategory(), array(2045611)); 	//11
-		$this->assertEquals($object->getContext()->getDo_not_track(), 2);    		 //13
-		$this->assertEquals($object->getContext()->getSubid(), 52193);    		 //19
-		$this->assertEquals($object->getContext()->getPublisher(), 418);    		 //27
-		$this->assertEquals($object->getContext()->getKeyword(), array(82427 => 11,8896 => 7,33453554 => 4,296087 => 3,56332 => 3, 689251 => 2, 27499 => 1, 32941772 => 1, 70764 => 1, 17128 => 0));    	 //33
-		$this->assertEquals($object->getContext()->getCategory_sem(), array(472441 => 255,472358 => 255, 472419 => 255));    		 //46
+		$this->assertEquals($object->getContext()->getGender(), array(7 => 255)); //1
+		$this->assertEquals($object->getContext()->getAge(), array(12, 13, 42, 90, 46, 29, 19)); //2
+		$this->assertEquals($object->getContext()->getIncome(), array(43, 24, 44, 105, 20, 16)); //3
+		$this->assertEquals($object->getContext()->getBrowser(), 312613); //4
+		$this->assertEquals($object->getContext()->getIsp(), 86); //5
+		$this->assertEquals($object->getContext()->getOs(), 431247); //6
+		$this->assertEquals($object->getContext()->getGeo_user(), null); //7
+		$this->assertEquals($object->getContext()->getPublisher_filter(), array(18841, 18842, 48511)); //8
+		$this->assertEquals($object->getContext()->getGeo_user(), null); //9
+		$this->assertEquals($object->getContext()->getChannel(), array(9, 10)); //10
+		$this->assertEquals($object->getContext()->getCategory(), array(2045611)); //11
+		$this->assertEquals($object->getContext()->getDo_not_track(), 2); //13
+		$this->assertEquals($object->getContext()->getSubid(), 52193); //19
+		$this->assertEquals($object->getContext()->getPublisher(), 418); //27
+		$this->assertEquals($object->getContext()->getKeyword(), array(82427 => 11, 8896 => 7, 33453554 => 4, 296087 => 3, 56332 => 3, 689251 => 2, 27499 => 1, 32941772 => 1, 70764 => 1, 17128 => 0)); //33
+		$this->assertEquals($object->getContext()->getCategory_sem(), array(472441 => 255, 472358 => 255, 472419 => 255)); //46
 
 		/**
 		 * testing recs values
 		 */
-		$this->assertEquals($object->getRecs()->getScores(), array(130106300 , 84799192));
+		$this->assertEquals($object->getRecs()->getScores(), array(130106300, 84799192));
 
 		/**
 		 * testing timestamp values
 		 */
 		$this->assertEquals($object->getTimestamp()->getTimestampValue(), 1372175999641);
-
 	}
 }
